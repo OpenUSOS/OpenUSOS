6a737f44ea182d2898a101e059426c3a62ae671f
oskarkulinski <oskarkulinski3@gmail.com>
2024-04-24 22:20:15 +0200

    Added an early version of settings

---
+++ lib/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  textTheme: TextTheme(
+  )
+);
+
+//We use the ThemData class provided by flutter
---
+++ lib/user_session.dart
@@ -0,0 +1,11 @@
+
+class Usersession {
+
+  static void login() {
+    throw UnimplementedError();
+  }
+
+  static void logout() {
+    throw UnimplementedError();
+  }
+}
