c0cf54b2b6080ec12d34fd0f83e97120d7675490
Wienio Wlodyga <wienczyslaw.wlodyga@student.uj.edu.pl>
2024-04-28 00:16:44 +0200

    added initial grades UI

--- lib/pages/grades.dart
+++ lib/pages/grades.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:open_usos/main.dart';
 
 
 class Grades extends StatefulWidget {
@@ -12,23 +11,35 @@ class Grades extends StatefulWidget {
 
 //we annotate it with visibleForTesting to make sure the state class isn't used anywhere else
 //we make it publics so that it can be tested
-@visibleForTesting
 class GradesState extends State<Grades> {
-  List termList = [];
-
-  //we call the superclass constructor and getData to initialize termList
-  GradesState() : super(){
-    getData();
+  List<Map<String, dynamic>> gradesData = [];
+  void initState() {
+    super.initState();
+    gradesData = getData();
   }
+  List<Map<String, dynamic>> getData() {
+    return [
+      {'term': '23/24 Z', 'subject': 'Programowanie', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '23/24 Z', 'subject': 'Grafika komputerowa', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '23/24 Z', 'subject': 'Nienawidze programowania 2', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '23/24 Z', 'subject': 'Wstęp do piekła', 'grade': 2.0, 'professor': 'Rafael Coffee'},
+      {'term': '22/23 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '22/23 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '21/22 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '21/22 L', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+      {'term': '21/22 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
+    ];
+  } 
 
-  //getting data from api
-  void getData() {
-    termList = [{"23/24" : 1}, {"24/25" : 2}];
-  }
-
-  //build method
   @override
   Widget build(BuildContext context) {
+    //zmienna przechowujaca pogrupowane oceny wzgledem semestrow
+    var groupedByTerms = {};
+
+    //grupujemy w nowej mapie map
+    for (var item in gradesData) 
+      groupedByTerms.putIfAbsent(item['term'], () => []).add(item); 
+
     return Scaffold(
       appBar: AppBar(
                 title: Text(
@@ -49,31 +60,58 @@ class GradesState extends State<Grades> {
           )
         ]
       ),
-      body: ListView.builder( //a list of terms
-        itemCount: termList.length,
-        itemBuilder: (context, index){
-          Map<String, dynamic> currentList = termList[index];
-          // Use the current list to build a row
-          return ListView.builder( // a list of grades in each term
-            itemCount: termList[index].length,
-            itemBuilder: (context2, index2) {
-d              return ListTile( // a grade from a singe class
-                  title: Text('List ${index + 1}'),
-                  subtitle: Row(
-                      children: [
-                        for (dynamic item in currentList.entries)
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0),
-                            child: Text('AAA'),
-                          ),
-                      ]
+      body: ListView.builder(
+        itemCount: groupedByTerms.entries.length,
+        itemBuilder: (context, index) {  
+          var term = groupedByTerms.entries.elementAt(index).key;
+          var gradeDetails = groupedByTerms.entries.elementAt(index).value;
+          
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text( 
+                  term,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0
                   )
-              );
-            }
+                )
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: gradeDetails.length, 
+                itemBuilder: (context, subIndex) {
+                  var item = gradeDetails[subIndex];
+                  return Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    child: ListTile(
+                      title: Text(item['subject']),
+                      subtitle: Text('given by ${item['professor']}'),
+                      leading: CircleAvatar(
+                        backgroundColor: item['grade'] == 2.0 ? Colors.redAccent[700] : Colors.blue[700],
+                        radius: 20.0,
+                        child: Text(
+                          '${item['grade']}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ), 
+                        ),
+                      ),
+                    )
+                  );
+                }
+              )
+            ],
           );
-        },
-      ),
+        }
+      )
     );
   }
 }
