cac6063b408475e95ec33982c53b287bf1b0e032
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-05-01 20:56:46 +0200

    Settings with (hopefully) working saving, need the visual side

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:open_usos/pages/home.dart';
 import 'package:open_usos/pages/grades.dart';
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,33 +1,89 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:open_usos/themes.dart';
 
-class Settings {
+class Settings extends StatefulWidget{
+  //set of available languages
+  final Set<String> availableLanguages = {'Polish'};
+  //map of available themes, they can be accessed by name
+  final Map<String, ThemeData> availableThemes = {"dark": darkTheme};
 
-  Set<String> availableLanguages = {'Polish'};
-  Set<ThemeData> availableThemes = {darkTheme};
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  //default settings here, if user has other settings set this will be overwritten
+  //in _initState setPreferances
   String currentLanguage = 'Polish';
   ThemeData currentTheme = darkTheme;
   bool notificationsOn = false;
 
-  Settings(String language, ThemeData theme, bool notifications) {
-    notifications = false;
-    currentTheme = theme;
-    currentLanguage = language;
+  @override
+  void initState() async {
+    super.initState();
+    _initPreferences();//we initialize preferences
   }
 
+  void _initPreferences() async{
+    // we get preferences and set them using SharedPreferences library
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setPreferences(prefs);
+  }
+
+  //if settings are saved defaults are overwritten, if not nothing happens
+  void setPreferences(SharedPreferences prefs){
+    bool? notifications = prefs.getBool('notifications');
+    if(notifications != null){
+      notificationsOn = notifications;
+    }
+
+    String? language = prefs.getString('language');
+    if(language != null){
+      setLanguage(language)
+    }
+
+    String? theme = prefs.getString('theme');
+    if(theme != null){
+      setTheme(theme);
+    }
+
+  }
+
+  // language setter with check if language is available
   void setLanguage(String language) {
-    if (availableLanguages.contains(language)) {
+    if (widget.availableLanguages.contains(language)) {
       currentLanguage = language;
     } else {
-      throw Error();
+      throw Exception('Language not available');
     }
   }
 
-  void setTheme(ThemeData theme) {
-    if (availableThemes.contains(theme)) {
-      currentTheme = theme;
+  //theme setter with check if theme is available
+  void setTheme(String themeName) {
+    if (widget.availableThemes.containsKey(themeName)) {
+      // we check if theme is available and set it
+      currentTheme = widget.availableThemes[themeName]!;
     } else {
-      throw Error();
+      throw Exception('Theme not available');
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ustawienia",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          )
+        )
+      ),
+    );
+  }
+
 }
