8a0deaae3255437936183651d06368813dc17ae5
Wienio Wlodyga <wienczyslaw.wlodyga@student.uj.edu.pl>
2024-05-02 18:18:27 +0200

    changed grades UI

--- lib/pages/grades.dart
+++ lib/pages/grades.dart
@@ -26,13 +26,15 @@ class GradesState extends State<Grades> {
       {'term': '22/23 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
       {'term': '22/23 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
       {'term': '21/22 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
-      {'term': '21/22 L', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
       {'term': '21/22 Z', 'subject': 'Sranie 1', 'grade': 4.5, 'professor': 'Rafael Coffee'},
     ];
   } 
 
   @override
   Widget build(BuildContext context) {
+
+    Color? failed = Colors.red[800];
+    Color? passed = Colors.blue[800];
     //zmienna przechowujaca pogrupowane oceny wzgledem semestrow
     var groupedByTerms = {};
 
@@ -42,11 +44,17 @@ class GradesState extends State<Grades> {
 
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        elevation: 5.0,
+        backgroundColor: Colors.blueGrey[900],
                 title: Text(
           "Your grades",
           style: TextStyle(
             fontWeight: FontWeight.bold,
-          ),
+            color: Colors.white,
+          ), 
         ),
         actions: <Widget>[
           IconButton(
@@ -56,7 +64,9 @@ class GradesState extends State<Grades> {
                 Navigator.pushNamed(context, '/home'); 
               };
             },
-            icon: Icon(Icons.home_filled,)
+            icon: Icon(Icons.home_filled,
+            color: Colors.white,
+            )
           )
         ]
       ),
@@ -69,6 +79,12 @@ class GradesState extends State<Grades> {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              Divider(
+                indent: 10.0,
+                endIndent: 10.0,
+                height: 10.0,
+                color: Colors.grey[400],
+              ),
               Padding(
                 padding: EdgeInsets.all(5.0),
                 child: Text( 
@@ -79,6 +95,12 @@ class GradesState extends State<Grades> {
                   )
                 )
               ),
+              Divider(
+                indent: 10.0,
+                endIndent: 10.0,
+                height: 10.0,
+                color: Colors.grey[400],
+              ),
               ListView.builder(
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
@@ -86,15 +108,20 @@ class GradesState extends State<Grades> {
                 itemBuilder: (context, subIndex) {
                   var item = gradeDetails[subIndex];
                   return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                     elevation: 4.0,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
                     child: ListTile(
-                      title: Text(item['subject']),
+                      title: Text(item['subject'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       subtitle: Text('given by ${item['professor']}'),
                       leading: CircleAvatar(
-                        backgroundColor: item['grade'] == 2.0 ? Colors.redAccent[700] : Colors.blue[700],
+                        backgroundColor: item['grade'] == 2.0 ? failed : passed,
                         radius: 20.0,
                         child: Text(
                           '${item['grade']}',
