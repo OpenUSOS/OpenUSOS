6fa3db871c3f1d40af7b68cc6a1aac2553a063b3
Oskar Kulinski <oskarkulinski3@gmail.com>
2024-05-02 21:02:28 +0200

    Redirecting to login website working

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:open_usos/pages/home.dart';
 import 'package:open_usos/pages/grades.dart';
 import 'package:open_usos/settings.dart';
+import 'package:open_usos/user_session.dart';
 
 void main() {
   runApp(OpenUSOS());
@@ -11,7 +14,12 @@ void main() {
 
 
 class OpenUSOS extends StatelessWidget {
-  const OpenUSOS({super.key});
+
+  OpenUSOS({super.key}){
+      //UserSession.createSession();
+      //sleep(Duration(seconds: 5));
+      UserSession.login();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/user_session.dart
+++ lib/user_session.dart
@@ -1,11 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class Usersession {
+class UserSession {
+  static String? sessionId;
+  static const host = 'srv27.mikr.us:20117';
+  static const logPath = '/login';
+  static const basePath = '/api';
 
-  static void login() {
-    throw UnimplementedError();
+  static Future createSession() async {
+    final url = Uri.http(host, logPath);
+    var response = await get(url);
+    if (response.statusCode == 200) {
+      sessionId = response.body;
+    }
+    else {
+      throw Exception('Connecting to server failed');
+    }
+  }
+
+
+  static void login() async {
+    await createSession();
+    final urlURL = Uri.http(host, basePath, {'id': sessionId, 'query1': 'url'});
+    var response = await get(urlURL);
+    String loginUrl;
+    if (response.statusCode == 200) {
+      loginUrl = response.body;
+    }
+    else {
+      throw Exception('Getting Login URL failed');
+    }
+    _launchURL(loginUrl); // opens web browser with the correct link
+
+    throw Exception(post(Uri.parse(loginUrl)));
   }
 
   static void logout() {
     throw UnimplementedError();
   }
+
+
+  static _launchURL(String loginUrl) async {
+    final url = Uri.parse(loginUrl);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 }
+
+
